98c669f59ba773b3e48b78e983dd885c75d4d940
Ahmad1026 <mohammadiahmad1026@gmail.com>
2020-12-10 13:03:24 +0330

    add a coppy midelwer

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use("/" ,(req,res,next) => {
     console.log("ahmad");
     res.sendFile(__dirname + '/statics/home.html'); 
 });
+app.use("/" ,(req,res,next) => {
+
+    console.log("ahmad");
+    res.sendFile(__dirname + '/statics/home.html'); 
+});
 
 
 
