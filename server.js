60a1e5f35d8d5d2eabae2173a3d2c4e5492b0dcc
Ahmad1026 <mohammadiahmad1026@gmail.com>
2020-12-10 12:55:42 +0330

    first chengs saved

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + '/statics'));
+
+
+
+app.use("/" ,(req,res,next) => {
+
+    console.log("ahmad");
+    res.sendFile(__dirname + '/statics/home.html'); 
+});
+
+
+
+app.listen(8000);
+console.log("server is rummimg in port 8000!!!");
